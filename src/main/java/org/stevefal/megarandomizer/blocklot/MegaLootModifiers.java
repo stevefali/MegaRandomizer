dffa8427f491622f04edd09c395b550104a2f0fc
stevefali <stevefaliszewski@gmail.com>
2023-08-23 14:11:14 -0700

    Added Block drop randomization

---
+++ src/main/java/org/stevefal/megarandomizer/blocklot/MegaLootModifiers.java
@@ -0,0 +1,22 @@
+package org.stevefal.megarandomizer.blocklot;
+
+import com.mojang.serialization.Codec;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.stevefal.megarandomizer.MegaRandomizer;
+
+public class MegaLootModifiers {
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, MegaRandomizer.MODID);
+
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> RANDOMIZE_BLOCK_DROPS =
+            LOOT_MODIFIER_SERIALIZERS.register("randomize_block_drops", BlockDropsModifier.BLOCK_CODEC);
+
+
+    public static void register(IEventBus bus) {
+        LOOT_MODIFIER_SERIALIZERS.register(bus);
+    }
+}
