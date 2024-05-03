fe8183e77ce60ab68a5b745e2eb7785a27216993
Till Hummel <hummel.till@web.de>
2024-05-03 14:27:08 +0200

    add gamerules and change how exclusions work

--- src/main/java/org/stevefal/megarandomizer/gamerules/MegaGameRules.java
+++ src/main/java/org/stevefal/megarandomizer/gamerules/MegaGameRules.java
@@ -7,6 +7,9 @@ public class MegaGameRules {
     public static GameRules.Key<GameRules.BooleanValue> RULE_DOBLOCKRANDOMDROPS;
     public static GameRules.Key<GameRules.BooleanValue> RULE_DOENTITYRANDOMDROPS;
     public static GameRules.Key<GameRules.BooleanValue> RULE_DOPLAYERRANDOMDROPS;
+    public static GameRules.Key<GameRules.BooleanValue> RULE_EXCLUDECREATIVEITEMS;
+    public static GameRules.Key<GameRules.BooleanValue> RULE_EXCLUDESPAWNEGGS;
+    public static GameRules.Key<GameRules.BooleanValue> RULE_EXCLUDEHEADS;
 
 
 
@@ -16,6 +19,12 @@ public class MegaGameRules {
         RULE_DOENTITYRANDOMDROPS = GameRules.register("doEntityRandomDrops", GameRules.Category.DROPS, GameRules.BooleanValue.create(true));
 
         RULE_DOPLAYERRANDOMDROPS = GameRules.register("doPlayerRandomDrops", GameRules.Category.DROPS, GameRules.BooleanValue.create(true));
+
+        RULE_EXCLUDECREATIVEITEMS = GameRules.register("excludeCreativeItems", GameRules.Category.DROPS, GameRules.BooleanValue.create(true));
+
+        RULE_EXCLUDESPAWNEGGS = GameRules.register("excludeSpawnEggs", GameRules.Category.DROPS, GameRules.BooleanValue.create(true));
+
+        RULE_EXCLUDEHEADS = GameRules.register("excludeHeads", GameRules.Category.DROPS, GameRules.BooleanValue.create(true));
 
     }
 
