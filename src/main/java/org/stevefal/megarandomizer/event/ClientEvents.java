d56c68bceed13f986c14c4fdeac497c0d0c22a85
stevefali <stevefaliszewski@gmail.com>
2026-09-11 22:11:13 -0700

    Remove now-unnecessary '/reshuffle' command

--- src/main/java/org/stevefal/megarandomizer/event/ClientEvents.java
+++ src/main/java/org/stevefal/megarandomizer/event/ClientEvents.java
@@ -27,14 +27,6 @@ public class ClientEvents {
             final Minecraft minecraft = Minecraft.getInstance();
             boolean isSinglePlayer = minecraft.isSingleplayer();
             if (event.getScreen() instanceof PauseScreen && !(event.getScreen() instanceof ModPauseScreen)) {
-                long seed = 0;
-                if (minecraft.hasSingleplayerServer() && minecraft.getSingleplayerServer() != null) {
-                    seed = minecraft.getSingleplayerServer().getWorldData().worldGenOptions().seed();
-                } else if (minecraft.level != null && minecraft.level.getServer() != null) {
-                    seed = minecraft.level.getServer().getWorldData().worldGenOptions().seed();
-                } else if (minecraft.player != null && minecraft.player.clientLevel.getServer() != null) {
-                    seed = minecraft.player.clientLevel.getServer().getWorldData().worldGenOptions().seed();
-                }
                 minecraft.setScreen(new ModPauseScreen(true, isSinglePlayer));
             }
         }
--- src/main/java/org/stevefal/megarandomizer/event/ServerEvents.java
+++ src/main/java/org/stevefal/megarandomizer/event/ServerEvents.java
@@ -10,15 +10,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.WorldData;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.MobSpawnEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.command.ConfigCommand;
 import org.stevefal.megarandomizer.MegaRandomizer;
-import org.stevefal.megarandomizer.commands.ReshuffleCommand;
 import org.stevefal.megarandomizer.gamerules.MegaGameRules;
 import org.stevefal.megarandomizer.megadata.MegaSavedDataAccess;
 import org.stevefal.megarandomizer.megadrops.RandomDrops;
@@ -41,7 +38,12 @@ public class ServerEvents {
         final boolean excludeSpawnEggs = gameRules.getBoolean(MegaGameRules.RULE_EXCLUDESPAWNEGGS);
         final boolean excludeHeads = gameRules.getBoolean(MegaGameRules.RULE_EXCLUDEHEADS);
         final boolean excludeBosses = gameRules.getBoolean(MegaGameRules.RULE_EXCLUDE_BOSSES);
-        RandomDrops.shuffleItems(worldData.worldGenOptions().seed(), excludeCreativeItems, excludeSpawnEggs, excludeHeads);
+        RandomDrops.shuffleItems(
+                worldData.worldGenOptions().seed(),
+                excludeCreativeItems,
+                excludeSpawnEggs,
+                excludeHeads
+        );
         RandomSpawns.shuffleEntities(worldData.worldGenOptions().seed(), excludeBosses);
     }
 
@@ -67,19 +69,16 @@ public class ServerEvents {
         ArrayList<ItemEntity> randomizedDrops = new ArrayList<>();
         event.getDrops().forEach(vanillaDrops -> {
             for (int i = 0; i < vanillaDrops.getItem().getCount(); i++) {
-                randomizedDrops.add(new ItemEntity(level, ent.getX(), ent.getY(), ent.getZ(), RandomDrops.getRandomizedItem(vanillaDrops.getItem(),
-                        level.getServer().getGameRules().getBoolean(MegaGameRules.RULE_DO_VOLATILE_DROPS))));
+                randomizedDrops.add(new ItemEntity(
+                        level, ent.getX(), ent.getY(), ent.getZ(), RandomDrops.getRandomizedItem(
+                        vanillaDrops.getItem(),
+                        level.getServer().getGameRules().getBoolean(MegaGameRules.RULE_DO_VOLATILE_DROPS)
+                )
+                ));
             }
         });
         event.getDrops().clear();
         event.getDrops().addAll(randomizedDrops);
-    }
-
-    @SubscribeEvent
-    public static void onCommandsRegister(RegisterCommandsEvent event) {
-        new ReshuffleCommand(event.getDispatcher());
-
-        ConfigCommand.register(event.getDispatcher());
     }
 
     @SubscribeEvent
